688362773ece3459e69b6e6f1971543e9a0e2820
alshrestha <alshrestha88@gmail.com>
2020-08-17 11:06:22 -0500

    Added Parameterized constructor

--- Student_Application/src/com/ds/Student.java
+++ Student_Application/src/com/ds/Student.java
@@ -3,56 +3,74 @@ package com.ds;
 import java.util.Date;
 
 public class Student {
-	
-	
+
 	private int id;
 	private String name;
 	private Date dob;
 	private int age;
 	private long contact;
 	private String address;
-	
-	public Date getDob() {
-		return dob;
+
+	public Student() {
+
+	}
+
+	public Student(int id, String name, Date dob, int age, long contact, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dob = dob;
+		this.age = age;
+		this.contact = contact;
+		this.address = address;
 	}
 
 	public long getContact() {
-		return dob;
+		return contact;
 	}
 
 	public void setContact(long contact) {
-		return contact;
+		this.contact = contact;
+	}
+
+	public Date getDob() {
+		return dob;
 	}
 
 	public void setDob(Date dob) {
 		this.dob = dob;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
-	
-	
 
 }
